47a18208aa9dd56ae39ff5f83a3fc1fc5786babb
Clo Yun-Hee Dufour <cloyunhee@gmail.com>
2021-11-08 09:01:33 +0100

    Few tweaks to the CMake files

--- cmake/packaging/Linux.cmake
+++ cmake/packaging/Linux.cmake
@@ -3,6 +3,6 @@ set(BIN_DESTINATION bin)
 set(LIB_DESTINATION lib)
 set(PLUGIN_DESTINATION plugins)
 set(PLATFORM_PLUGINS qxcb)
-set(AUDIO_PLUGINS audio_alsa media_pulse)
+set(AUDIO_PLUGINS qtaudio_alsa qtmedia_pulse)
 
 set(RUNTIME_DEPS FALSE)
