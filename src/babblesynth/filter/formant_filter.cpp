be679e6c43ca7dbeb4b48b60fb350a4c044c6673
Clo Yun-Hee Dufour <cloyunhee@gmail.com>
2021-12-05 23:42:45 +0100

    Fix mistake missing argument

--- src/babblesynth/filter/formant_filter.cpp
+++ src/babblesynth/filter/formant_filter.cpp
@@ -36,11 +36,11 @@ formant_filter::formant_filter(int sampleRate)
       m_F4(true),
       m_F5(true),
       m_sampleRate(sampleRate) {
-    addParameter("F1 plan", variable_plan(1000));
-    addParameter("F2 plan", variable_plan(1300));
-    addParameter("F3 plan", variable_plan(2400));
-    addParameter("F4 plan", variable_plan(2900));
-    addParameter("F5 plan", variable_plan(3800));
+    addParameter("F1 plan", variable_plan(true, 1000));
+    addParameter("F2 plan", variable_plan(true, 1300));
+    addParameter("F3 plan", variable_plan(true, 2400));
+    addParameter("F4 plan", variable_plan(true, 2900));
+    addParameter("F5 plan", variable_plan(true, 3800));
 }
 
 std::vector<double> formant_filter::generateFrom(
